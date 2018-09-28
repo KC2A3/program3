72a83eb07924a297598241b26c40299c7555081f
KC2A3 <947496351@qq.com>
2018-09-28 21:36:56 +0800

    第n次修改

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
-module.exports = function main() {
-  // Write your code here
-};
+
+  function newway(anarray) {
+    var result = 0;
+    for(var i = 0; i < anarray.length; i++) {
+     result += anarray[i];
+    }
+    return result;
+   };
+   module.exports = { newway }
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -3,8 +3,8 @@ const main = require('../main/main');
 describe('main()', () => {
 
     it('should calculate the sum', () => {
-      expect(main("123")).toBe(6);
-      expect(main("102")).toBe(3);
+      expect(main.newway([1,2,3])).toBe(6);
+      expect(main.newway([1,0,2])).toBe(3);
     });
 
 });
